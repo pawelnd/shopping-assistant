04f9d6bacbda50513b100a43743a9c2f1d757862
pzych <pawel.zych@gmail.com>
2020-09-13 22:58:25 +0200

    started basic layout

---
+++ packages/client/src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Index() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default Index;
