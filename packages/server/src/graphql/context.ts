85a27d085718ba725c2c158a45b450938850cdcc
pzych <pawel.zych@gmail.com>
2021-03-08 22:18:57 +0100

    graphql

--- packages/server/src/graphql/context.ts
+++ packages/server/src/graphql/context.ts
@@ -1,18 +1,15 @@
-import { Container } from 'typedi';
 import express from 'express';
 import User from '../common/interfaces/user.interface';
 
 export interface Context {
   req: express.Request;
   res: express.Response;
-  injector: Container;
   currentUser?: User;
 }
 
 export const buildContext = (
   contextExtra: Pick<Context, 'req' | 'res'> & Partial<Context>,
 ): Context => ({
-  injector: Container,
   currentUser: undefined,
   ...contextExtra,
 });
